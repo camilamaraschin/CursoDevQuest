5711babc6d7c70f8c3ff5a94de156c323b98e416
Camila Maraschin <105385268+camilamaraschin@users.noreply.github.com>
2022-12-12 08:14:56 -0300

    Add files via upload

---
+++ Atividade JSIntermediario/Ativ 2/main.js
@@ -0,0 +1,7 @@
+const multiplicar = [65, 44, 12, 4];
+
+multiplicar.forEach((item) => {
+		
+		console.log(item* 10);
+		
+});
---
+++ Atividade JSIntermediario/Ativ 3/main.js
@@ -0,0 +1,13 @@
+const quadrado = document.querySelector(".quadrado");
+
+quadrado.addEventListener('click', () => {
+    const classeAzulExiste = quadrado.classList.contains('azul');
+    if (classeAzulExiste) {
+        quadrado.classList.remove("azul");
+    } else if  {
+				
+        quadrado.classList.add("azul"){
+
+            alert("Esse quadrado TEM a classe azul!");
+               }
+        }
